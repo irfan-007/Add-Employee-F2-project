eb4a28953e6bb3a946fe721eb8241bc0598fff73
irfan-007 <thanatos786687@gmail.com>
2024-08-03 19:29:09 +0530

    all completed

--- script.js
+++ script.js
@@ -1,30 +1,62 @@
-// DOM elements --
-const circles = document.querySelectorAll(".circle");
-let progress = document.querySelector(".indicator");
-let prev = document.getElementById("prev");
-let next = document.getElementById("next");
+// variables ---
+const userList = [];
+let submitButton = document.getElementById("btn-add");
+let userId = 0;
+let messageErr = document.querySelector(".message.m1");
+let messageSucc = document.querySelector(".message.m2");
+let content = document.querySelector(".content");
+let messageTem = document.querySelector(".tem");
 
-// console.log(circles, progress, prev, next);
-let max_step = circles.length - 1;
-let curr_step = 0;
-let multi = 100 / max_step;
-// logical functions ---
-prev.addEventListener("click", (e) => {
-  if (curr_step == 0) return;
-  let circle = circles[curr_step];
-  circle.classList["remove"]("active");
-  curr_step--;
-  progress.style.width = `${multi * curr_step}%`;
-  if (curr_step == 0) prev.disabled = true;
-  next.disabled = false;
-});
+// add events
+submitButton.addEventListener("click", addUser);
 
-next.addEventListener("click", (e) => {
-  if (curr_step == max_step) return;
-  curr_step++;
-  let circle = circles[curr_step];
-  circle.classList["add"]("active");
-  progress.style.width = `${multi * curr_step}%`;
-  prev.disabled = false;
-  if (curr_step == max_step) next.disabled = true;
-});
+// logical functions
+function addUser(e) {
+  e.preventDefault();
+  let name = document.getElementById("name");
+  let age = document.getElementById("age");
+  let profession = document.getElementById("profession");
+  if (name.value == "" || age.value == "" || profession.value == "") {
+    messageErr.style.display = "block";
+    messageSucc.style.display = "none";
+    return;
+  }
+
+  userId++;
+  userList.push({
+    id: userId,
+    name: name.value,
+    profession: profession.value,
+    age: age.value,
+  });
+  messageErr.style.display = "none";
+  messageSucc.style.display = "block";
+  console.log(userList);
+  userMap();
+}
+
+function userMap() {
+  content.innerHTML = "";
+  if (userList.length == 0) messageTem.style.display = "block";
+  else messageTem.style.display = "none";
+
+  userList.map((obj, idx) => {
+    let id = obj.id;
+    let name = obj.name;
+    let age = obj.age;
+    let profession = obj.profession;
+    let div = document.createElement("div");
+    div.id = "user" + id;
+    div.innerHTML = `<div class="user-details"><span>${
+      idx + 1
+    }.</span><span>Name : ${name}</span> <span>Profession : ${profession}</span><span>Age : ${age}</span></div> <button class="btn-del" onClick="deleteUser(${id},${idx})">Delete User</button>`;
+    content.appendChild(div);
+  });
+}
+
+function deleteUser(id, idx) {
+  console.log(id);
+  let node = document.getElementById("user" + id);
+  userList.splice(idx, 1);
+  userMap();
+}
